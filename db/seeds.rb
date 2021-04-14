594caef6319da113b59d97cda3f9706f4e96f6c5
Daniel Pescador <df.pescador@gmail.com>
2021-04-14 09:06:18 -0500

    Refactor to make the code into functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,16 @@
 require 'faker'
 
 
-OrderState.destroy_all
-Province.destroy_all
-Customer.destroy_all
-Book.destroy_all
-Genre.destroy_all
-Author.destroy_all
-Order.destroy_all
-BooksOrder.destroy_all
-
+def delete_all_content()
+  OrderState.destroy_all
+  Province.destroy_all
+  Customer.destroy_all
+  Book.destroy_all
+  Genre.destroy_all
+  Author.destroy_all
+  Order.destroy_all
+  BooksOrder.destroy_all
+end
 # SINGLE CREATION
 
 # horror = Genre.create(name: 'Horror')
@@ -29,67 +30,69 @@ BooksOrder.destroy_all
 
 # new_books_order = BooksOrder.create(book: stories, order:new_order, price:stories.price, quantity: 1 )
 
-# Create all order states
-all_order_states = ['PENDING', 'AWAITING PAYMENT', 'AWAITING FULFILLMENT', 'COMPLETED']
+def generate_content()
+  # Create all order states
+  all_order_states = ['PENDING', 'AWAITING PAYMENT', 'AWAITING FULFILLMENT', 'COMPLETED']
 
-all_order_states.each do |state|
-  OrderState.find_or_create_by(name: state)
+  all_order_states.each do |state|
+    OrderState.find_or_create_by(name: state)
+  end
+
+  # Create provinces
+  all_provinces = []
+  all_provinces_short_name = ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT']
+
+  all_provinces_short_name.each do |province, index|
+    new_province = Province.create(name: province)
+    all_provinces << new_province
+  end
+
+  # Create customers
+  all_customers = []
+  4.times do |index|
+  customer_name = Faker::Name.unique.name
+  new_customer = Customer.find_or_create_by(
+    name: customer_name,
+    email: Faker::Internet.email(name:customer_name, separators:'.'),
+    address: Faker::Address.unique.street_address,
+    postal_code: Faker::Address.unique.postcode,
+    province: all_provinces.sample
+    )
+  end
+
+  # Generate 10 genres
+  all_genres = []
+  4.times do |index|
+    new_genre = Genre.find_or_create_by(name: Faker::Book.unique.genre)
+    all_genres << new_genre
+  end
+
+
+  all_authors = []
+  10.times do |index|
+    new_author = Author.find_or_create_by(name: Faker::Book.unique.author)
+    all_authors << new_author
+  end
+
+  all_books = []
+  100.times do |index|
+    new_book = Book.find_or_create_by(
+      name: Faker::Book.unique.title,
+      author: all_authors.sample,
+      genre: all_genres.sample,
+      price: Faker::Number.decimal(l_digits: 2),
+      description: Faker::GreekPhilosophers.quote,
+      pages: Faker::Number.number(digits: 3))
+    all_books << new_book
+  end
+
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 end
 
-# Create provinces
-all_provinces_short_name = ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT']
-
-all_provinces = []
-all_provinces_short_name.each do |province, index|
-  new_province = Province.create(name: province)
-  all_provinces << new_province
-end
-
-# Create customers
-all_customers = []
-4.times do |index|
-customer_name = Faker::Name.unique.name
-new_customer = Customer.find_or_create_by(
-  name: customer_name,
-  email: Faker::Internet.email(name:customer_name, separators:'.'),
-  address: Faker::Address.unique.street_address,
-  postal_code: Faker::Address.unique.postcode,
-  province: all_provinces.sample
-  )
-end
-
-# Generate 10 genres
-all_genres = []
-4.times do |index|
-  new_genre = Genre.find_or_create_by(name: Faker::Book.unique.genre)
-  all_genres << new_genre
-end
-
-
-all_authors = []
-10.times do |index|
-  new_author = Author.find_or_create_by(name: Faker::Book.unique.author)
-  all_authors << new_author
-end
-
-all_books = []
-100.times do |index|
-  new_book = Book.find_or_create_by(
-    name: Faker::Book.unique.title,
-    author: all_authors.sample,
-    genre: all_genres.sample,
-    price: Faker::Number.decimal(l_digits: 2),
-    description: Faker::GreekPhilosophers.quote,
-    pages: Faker::Number.number(digits: 3))
-  all_books << new_book
-end
+# delete_all_content()
+# generate_content()
 
 
 
 
 
-
-
-
-
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
