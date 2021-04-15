6d0c8af770574c39ee67b5751e0d7d3d522121e4
Daniel Pescador <df.pescador@gmail.com>
2021-04-15 16:07:09 -0500

    Created controller for genres.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :author
   belongs_to :genre
-  # has_many :orders, through: :books_orders
+  # has_many :orders, through: :books_orders_order_id
   has_and_belongs_to_many :books_orders
+  accepts_nested_attributes_for :books_orders
 
   validates :name, presence: true
   validates_uniqueness_of :name
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,8 @@ class Order < ApplicationRecord
   # has_and_belongs_to_many :books
   belongs_to :order_state
   belongs_to :customer
-  # has_many :books, through: :books_order
+  # has_many :books, through: :books_orders_book_id
   has_and_belongs_to_many :books_orders
 
-  accepts_nested_attributes_for :books_orders, allow_destroy: true
+  accepts_nested_attributes_for :books_orders
 end
